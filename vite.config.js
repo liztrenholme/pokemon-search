e2b8c5edac1a5749e355028fff4bfff367319048
liztrenholme <liztrenholme@gmail.com>
2025-03-08 23:28:53 -0500

    upgrades to vite from ejected CRA base

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/pokemon-search',
+  plugins: [
+    react(), 
+    viteTsconfigPaths(),
+    svgr({
+      include: '**/*.svg?react',
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setup.js',
+    coverage: {
+      enabled: true,
+      reporter: ['text'],
+      thresholds: {
+        functions: 80,
+        branches: 80,
+        statements: 80
+      },
+      exclude: [
+        '.eslintrc',
+        '*config*',
+        'dist/**',
+        'public**/**',
+        '**/node_modules/**',
+        '**/dist/**',
+        'scripts/**',
+        'src/index.jsx',
+        'src/serviceWorker.js'
+      ]
+    }
+  }
+});
